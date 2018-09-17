ea100b572ee80201818bff79e7dd5d53e3ddeeda
Valter <valterjpcaldeira@gmail.com>
2018-09-17 23:41:58 +0100

    add firebase

--- dream-team-angular/src/environments/environment.ts
+++ dream-team-angular/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyBcO2YviiaYy-_QbgZxIWVQXRXHlkrKBPk",
+      authDomain: "dream-team-9b569.firebaseapp.com",
+      databaseURL: "https://dream-team-9b569.firebaseio.com",
+      projectId: "dream-team-9b569",
+      storageBucket: "dream-team-9b569.appspot.com",
+      messagingSenderId: "1091545225480"
+    }
 };
 
 /*
