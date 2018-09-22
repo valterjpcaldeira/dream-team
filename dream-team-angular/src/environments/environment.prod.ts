9ab1050a558ece50dc2236fdb03c116071df9bc7
Valter Caldeira <valterjpcaldeira@gmail.com>
2018-09-22 10:18:23 +0100

    Revert "delete stuff"
    This reverts commit fc09842b53a63d97f5b400687562d1adf1d0f498.

---
+++ dream-team-angular/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyBcO2YviiaYy-_QbgZxIWVQXRXHlkrKBPk",
+      authDomain: "dream-team-9b569.firebaseapp.com",
+      databaseURL: "https://dream-team-9b569.firebaseio.com",
+      projectId: "dream-team-9b569",
+      storageBucket: "dream-team-9b569.appspot.com",
+      messagingSenderId: "1091545225480"
+    }
+};
